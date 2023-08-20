2ae843b9ccef1fe5934d96ffe9f55575f4d2fedd
Maxim Markin <mak22223@ya.ru>
2023-08-20 09:31:02 +0300

    Made environment and simple scoreboard

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,19 @@
 #include "timer.hpp"
 #include "bird_character.hpp"
 
+/* TODO:
+ * - make x coord scalable as y coord
+ * -
+ */
+
 #define BTN_PIN 2
 #define OLED_CS 10
 #define OLED_RST 9
 #define OLED_DC 8
+
+const unsigned char tree_1 [] PROGMEM = {
+	0x02, 0x04, 0x3f, 0x10, 0x08
+};
 
 /* Input button flags */
 #define JUMP_BUTTON_FLAG 0x01
@@ -19,10 +28,10 @@
 #define USER_INPUT_CHECK_PERIOD 10
 
 /* Game rule constants */
-#define MAX_HEIGHT (16U << 8)
-#define MIN_HEIGHT (50U << 8)
-#define JUMP_ACCEL (2U << 8)
-#define GRAVITY_ACCEL (2U << 4)
+#define MAX_HEIGHT (16U)
+#define MIN_HEIGHT (61U)
+#define JUMP_ACCEL (1U << 8)
+#define GRAVITY_ACCEL (1U << 4)
 
 GyverOLED<SSD1306_128x64, OLED_BUFFER, OLED_SPI, OLED_CS, OLED_DC, OLED_RST> oled;
 
@@ -31,6 +40,7 @@ Timer userInputTimer(USER_INPUT_CHECK_PERIOD);
 
 Bird bird;
 
+long gameScore = 0;
 uint8_t inputFlags = 0;
 
 void moveBird(uint8_t input)
@@ -44,12 +54,12 @@ void moveBird(uint8_t input)
   posY = posY + bird.getVelY();
   bird.setVelY(bird.getVelY() + GRAVITY_ACCEL);
 
-  if (posY < MAX_HEIGHT) {
-    posY = MAX_HEIGHT;
+  if (posY < (MAX_HEIGHT << 8)) {
+    posY = (MAX_HEIGHT << 8);
     bird.setVelY(0);
   }
-  if (posY > MIN_HEIGHT) {
-    posY = MIN_HEIGHT;
+  if (posY > ((MIN_HEIGHT - bird.getSizeY()) << 8)) {
+    posY = ((MIN_HEIGHT - bird.getSizeY()) << 8);
     bird.setVelY(0);
   }
   bird.setPosY(posY);
@@ -57,8 +67,42 @@ void moveBird(uint8_t input)
 
 void drawEnvironment()
 {
-  // static uint8_t state = 0;
-  // oled.dot(10 - state, (MIN_HEIGHT >> 8))
+  static uint8_t state = 0;
+  oled.drawBitmap(100 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
+  oled.drawBitmap(100 - 64 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
+  oled.drawBitmap(100 + 64 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
+  oled.dot(10 - state, MIN_HEIGHT + 1);
+  oled.dot(12 - state, MIN_HEIGHT + 2);
+  oled.dot(25 - state, MIN_HEIGHT + 1);
+  oled.dot(24 - state, MIN_HEIGHT + 2);  
+  oled.dot(40 - state, MIN_HEIGHT + 1);
+  oled.dot(41 - state, MIN_HEIGHT + 2);
+  oled.dot(55 - state, MIN_HEIGHT + 1);
+  oled.dot(55 - state, MIN_HEIGHT + 2);
+  oled.dot(64 + 10 - state, MIN_HEIGHT + 1);
+  oled.dot(64 + 12 - state, MIN_HEIGHT + 2);
+  oled.dot(64 + 25 - state, MIN_HEIGHT + 1);
+  oled.dot(64 + 24 - state, MIN_HEIGHT + 2);  
+  oled.dot(64 + 40 - state, MIN_HEIGHT + 1);
+  oled.dot(64 + 41 - state, MIN_HEIGHT + 2);
+  oled.dot(64 + 55 - state, MIN_HEIGHT + 1);
+  oled.dot(64 + 55 - state, MIN_HEIGHT + 2);
+  oled.dot(128 + 10 - state, MIN_HEIGHT + 1);
+  oled.dot(128 + 12 - state, MIN_HEIGHT + 2);
+  oled.dot(128 + 25 - state, MIN_HEIGHT + 1);
+  oled.dot(128 + 24 - state, MIN_HEIGHT + 2);  
+  oled.dot(128 + 40 - state, MIN_HEIGHT + 1);
+  oled.dot(128 + 41 - state, MIN_HEIGHT + 2);
+  oled.dot(128 + 55 - state, MIN_HEIGHT + 1);
+  oled.dot(128 + 55 - state, MIN_HEIGHT + 2);
+  state = state > 63 ? 0 : state + 1;
+}
+
+void drawScoreboard()
+{
+  oled.setCursor(2, 0);
+  oled.print("Score: ");
+  oled.print(gameScore >> 4);
 }
 
 void setup() {
@@ -70,24 +114,12 @@ void setup() {
   oled.clear();   // очистить дисплей (или буфер)
   oled.update();  // обновить. Только для режима с буфером! OLED_BUFFER
 
-  bird.setPosXY(4, 45 << 8);
-
-  // oled.home();            // курсор в 0,0
-  // oled.print("Hello!");   // печатай что угодно: числа, строки, float, как Serial!
-  // oled.update();
-  // delay(5000);
-
-  // oled.drawBitmap(48, 16, bitmap_bird, 16, 13, BITMAP_NORMAL, BUF_ADD);
-  //oled.drawBitmap(90, 16, bitmap_32x32, 32, 32);  // по умолч. нормал и BUF_ADD
-  // x, y, имя, ширина, высота, BITMAP_NORMAL(0)/BITMAP_INVERT(1), BUF_ADD/BUF_SUBTRACT/BUF_REPLACE
-  
-  // oled.update();
+  bird.setPosXY(4 << 8, 45 << 8);
 }
 
 void loop() {
   if (userInputTimer.check()) {
     static bool lastBtnState = HIGH;
-
     userInputTimer.reset();
 
     bool userBtnState = digitalRead(BTN_PIN);
@@ -95,7 +127,6 @@ void loop() {
       lastBtnState = userBtnState;
       if (userBtnState == LOW) {
         inputFlags |= JUMP_BUTTON_FLAG;
-        Serial.println("Btn pressed");
       }
     }
   }
@@ -103,15 +134,10 @@ void loop() {
   if (gameTimer.check()) {
     gameTimer.reset();
 
+    gameScore += 1;
     moveBird(inputFlags);
 
     inputFlags = 0;
-
-    // static bool dir = true;
-    // if (bird.getPosY() == 16 || bird.getPosY() == 45) {
-    //   dir = !dir;
-    // }
-    // dir ? bird.setPosY(bird.getPosY() + 1) : bird.setPosY(bird.getPosY() - 1);
   }
 
   static long displayTimer = millis();
@@ -119,16 +145,12 @@ void loop() {
     displayTimer = millis();
 
     oled.clear();
-    oled.fastLineH((MIN_HEIGHT >> 8) + 11, 0, 127);
-    // drawEnvironment();
-    oled.drawBitmap(bird.getPosX(), bird.getPosY() >> 8, (uint8_t *)bird.getBitmap(), bird.getSizeX(), bird.getSizeY(), BITMAP_NORMAL, BUF_ADD);
-    // static bool flag = false;
-    // if (flag) {
-    //   oled.drawBitmap(charPosX, charPosY, bitmap_bird, 16, 11, BITMAP_NORMAL, BUF_ADD);
-    // } else {
-    //   oled.rect(charPosX, charPosY, charPosX + 16, charPosY + 11);
-    // }
-    // flag = !flag;
+    oled.fastLineH(MAX_HEIGHT - 3, 0, 127);
+    oled.fastLineH(MIN_HEIGHT, 0, 127);
+    drawScoreboard();
+    drawEnvironment();
+    oled.drawBitmap(bird.getPosX() >> 8, bird.getPosY() >> 8, (uint8_t *)bird.getBitmap(), bird.getSizeX(), bird.getSizeY(), BITMAP_NORMAL, BUF_ADD);
+
     oled.update();
   }
 }
