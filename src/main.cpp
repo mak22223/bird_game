1fd3e031c16596761645328a46920f8ed8ed39e4
Maxim Markin <mak22223@ya.ru>
2023-08-20 13:41:45 +0300

    Added moving obstacles

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,16 @@ const unsigned char tree_1 [] PROGMEM = {
 
 /* Game time periods */
 #define GAME_TICK_PERIOD 10
-#define DISPLAY_REFRESH_PERIOD 33
+#define DISPLAY_REFRESH_RATE 30
 #define USER_INPUT_CHECK_PERIOD 10
 
 /* Game rule constants */
-#define MAX_HEIGHT (16U)
-#define MIN_HEIGHT (61U)
+#define MAX_BIRD_HEIGHT (16U)
+#define MIN_BIRD_HEIGHT (61U)
+#define MAX_OBSTACLE_COUNT (3U)
+#define MAX_OBSTACLE_HEIGHT (20U)
+#define MIN_OBSTACLE_HEIGHT (55U)
+#define OBSTACLE_WINDOWN_HEIGHT (25U)
 #define JUMP_ACCEL (1U << 8)
 #define GRAVITY_ACCEL (1U << 4)
 
@@ -37,15 +41,30 @@ GyverOLED<SSD1306_128x64, OLED_BUFFER, OLED_SPI, OLED_CS, OLED_DC, OLED_RST> ole
 
 Timer gameTimer(GAME_TICK_PERIOD);
 Timer userInputTimer(USER_INPUT_CHECK_PERIOD);
+Timer displayTimer(1000 / DISPLAY_REFRESH_RATE);
+Timer debugTimer(1000);
 
 Bird bird;
 
+/* Game variables */
 long gameScore = 0;
+int gameSpeed = 100;
+long globalPos = 250;
 uint8_t inputFlags = 0;
+
+struct Obstacle
+{
+  long x;
+  uint8_t y;
+};
+uint8_t obstacleCount = 0;
+uint8_t lastObstacle = 0;
+Obstacle obstacles[MAX_OBSTACLE_COUNT];
+
 
 void moveBird(uint8_t input)
 {
-  int posY = bird.getPosY();
+  unsigned int posY = bird.getPosY();
 
   if (inputFlags & JUMP_BUTTON_FLAG) {
     bird.setVelY(-(JUMP_ACCEL));
@@ -54,55 +73,98 @@ void moveBird(uint8_t input)
   posY = posY + bird.getVelY();
   bird.setVelY(bird.getVelY() + GRAVITY_ACCEL);
 
-  if (posY < (MAX_HEIGHT << 8)) {
-    posY = (MAX_HEIGHT << 8);
+  if (posY < (MAX_BIRD_HEIGHT << 8)) {
+    posY = (MAX_BIRD_HEIGHT << 8);
     bird.setVelY(0);
   }
-  if (posY > ((MIN_HEIGHT - bird.getSizeY()) << 8)) {
-    posY = ((MIN_HEIGHT - bird.getSizeY()) << 8);
+  if (posY > ((MIN_BIRD_HEIGHT - bird.getSizeY()) << 8)) {
+    posY = ((MIN_BIRD_HEIGHT - bird.getSizeY()) << 8);
     bird.setVelY(0);
   }
   bird.setPosY(posY);
 }
 
+void processObstacles()
+{
+  /* Generating obstacles */
+  if (obstacleCount < MAX_OBSTACLE_COUNT) {
+    if (obstacleCount == 0 || (globalPos - obstacles[lastObstacle].x >= (50L << 8))) {
+      uint8_t y = random(MAX_OBSTACLE_HEIGHT, MIN_OBSTACLE_HEIGHT - OBSTACLE_WINDOWN_HEIGHT);
+
+      ++obstacleCount;
+      lastObstacle = (lastObstacle == 2) ? 0 : lastObstacle + 1;
+      obstacles[lastObstacle].x = globalPos & 0xFFFFFF00;
+      obstacles[lastObstacle].y = y;
+    }
+  }
+
+  /* Moving osbstacles */
+  for (uint8_t i = 0; i < obstacleCount; ++i) {
+    uint8_t index = i > lastObstacle ? MAX_OBSTACLE_COUNT - (i - lastObstacle) : lastObstacle - i;
+
+    if ((globalPos - obstacles[index].x) > (152L << 8)) {
+      --obstacleCount;
+    }
+  }
+}
+
+bool checkCollision()
+{
+  
+}
+
 void drawEnvironment()
 {
-  static uint8_t state = 0;
-  oled.drawBitmap(100 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
-  oled.drawBitmap(100 - 64 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
-  oled.drawBitmap(100 + 64 - state, MIN_HEIGHT - 6, tree_1, 5, 6);
-  oled.dot(10 - state, MIN_HEIGHT + 1);
-  oled.dot(12 - state, MIN_HEIGHT + 2);
-  oled.dot(25 - state, MIN_HEIGHT + 1);
-  oled.dot(24 - state, MIN_HEIGHT + 2);  
-  oled.dot(40 - state, MIN_HEIGHT + 1);
-  oled.dot(41 - state, MIN_HEIGHT + 2);
-  oled.dot(55 - state, MIN_HEIGHT + 1);
-  oled.dot(55 - state, MIN_HEIGHT + 2);
-  oled.dot(64 + 10 - state, MIN_HEIGHT + 1);
-  oled.dot(64 + 12 - state, MIN_HEIGHT + 2);
-  oled.dot(64 + 25 - state, MIN_HEIGHT + 1);
-  oled.dot(64 + 24 - state, MIN_HEIGHT + 2);  
-  oled.dot(64 + 40 - state, MIN_HEIGHT + 1);
-  oled.dot(64 + 41 - state, MIN_HEIGHT + 2);
-  oled.dot(64 + 55 - state, MIN_HEIGHT + 1);
-  oled.dot(64 + 55 - state, MIN_HEIGHT + 2);
-  oled.dot(128 + 10 - state, MIN_HEIGHT + 1);
-  oled.dot(128 + 12 - state, MIN_HEIGHT + 2);
-  oled.dot(128 + 25 - state, MIN_HEIGHT + 1);
-  oled.dot(128 + 24 - state, MIN_HEIGHT + 2);  
-  oled.dot(128 + 40 - state, MIN_HEIGHT + 1);
-  oled.dot(128 + 41 - state, MIN_HEIGHT + 2);
-  oled.dot(128 + 55 - state, MIN_HEIGHT + 1);
-  oled.dot(128 + 55 - state, MIN_HEIGHT + 2);
-  state = state > 63 ? 0 : state + 1;
+  uint8_t state = (globalPos >> 8) & 0x3F;
+  oled.drawBitmap(100 - state, MIN_BIRD_HEIGHT - 6, tree_1, 5, 6);
+  oled.drawBitmap(100 - 64 - state, MIN_BIRD_HEIGHT - 6, tree_1, 5, 6);
+  oled.drawBitmap(100 + 64 - state, MIN_BIRD_HEIGHT - 6, tree_1, 5, 6);
+  oled.dot(10 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(12 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(25 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(24 - state, MIN_BIRD_HEIGHT + 2);  
+  oled.dot(40 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(41 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(55 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(55 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(64 + 10 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(64 + 12 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(64 + 25 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(64 + 24 - state, MIN_BIRD_HEIGHT + 2);  
+  oled.dot(64 + 40 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(64 + 41 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(64 + 55 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(64 + 55 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(128 + 10 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(128 + 12 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(128 + 25 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(128 + 24 - state, MIN_BIRD_HEIGHT + 2);  
+  oled.dot(128 + 40 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(128 + 41 - state, MIN_BIRD_HEIGHT + 2);
+  oled.dot(128 + 55 - state, MIN_BIRD_HEIGHT + 1);
+  oled.dot(128 + 55 - state, MIN_BIRD_HEIGHT + 2);
 }
 
 void drawScoreboard()
 {
-  oled.setCursor(2, 0);
-  oled.print("Score: ");
+  oled.setCursorXY(2, 5);
+  oled.print("Score:");
   oled.print(gameScore >> 4);
+  oled.setCursorXY(70, 5);
+  oled.print("Best:");
+  oled.print(obstacleCount);
+  oled.fastLineH(MAX_BIRD_HEIGHT - 1, 0, 127);
+}
+
+void drawObstacles()
+{
+  for (uint8_t i = 0; i < obstacleCount; ++i) {
+    uint8_t index = i > lastObstacle ? MAX_OBSTACLE_COUNT - (i - lastObstacle) : lastObstacle - i;
+
+    int posX = 140 - (uint8_t)((globalPos - obstacles[index].x) >> 8);
+    oled.rect(posX, obstacles[index].y, posX + 12, MAX_BIRD_HEIGHT, 1);
+    oled.rect(posX, obstacles[index].y + OBSTACLE_WINDOWN_HEIGHT, posX + 12, MIN_BIRD_HEIGHT, 1);
+  }
 }
 
 void setup() {
@@ -134,23 +196,31 @@ void loop() {
   if (gameTimer.check()) {
     gameTimer.reset();
 
-    gameScore += 1;
     moveBird(inputFlags);
+    processObstacles();
+    checkCollision();
 
+    gameScore += 1;
+    globalPos += gameSpeed;
     inputFlags = 0;
   }
 
-  static long displayTimer = millis();
-  if (millis() - displayTimer >= DISPLAY_REFRESH_PERIOD) {
-    displayTimer = millis();
+  if (displayTimer.check()) {
+    displayTimer.reset();
 
     oled.clear();
-    oled.fastLineH(MAX_HEIGHT - 3, 0, 127);
-    oled.fastLineH(MIN_HEIGHT, 0, 127);
+    oled.fastLineH(MAX_BIRD_HEIGHT, 0, 127);
+    oled.fastLineH(MIN_BIRD_HEIGHT, 0, 127);
     drawScoreboard();
     drawEnvironment();
+    drawObstacles();
     oled.drawBitmap(bird.getPosX() >> 8, bird.getPosY() >> 8, (uint8_t *)bird.getBitmap(), bird.getSizeX(), bird.getSizeY(), BITMAP_NORMAL, BUF_ADD);
 
     oled.update();
+  }
+
+  if (debugTimer.check()) {
+    debugTimer.reset();
+
   }
 }
